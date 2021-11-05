7cb1381a217485b5c6ac45347826b069982edff9
Hamza BENMACHI <benmachi.hamza@gmail.Com>
2021-11-05 15:40:13 +0100

    Displayong the fetches users in the browser

--- api/Startup.cs
+++ api/Startup.cs
@@ -19,7 +19,7 @@ namespace api
     public class Startup
     {
         private readonly IConfiguration _config;
-
+       
         public Startup(IConfiguration config)
         {
           
@@ -41,6 +41,16 @@ namespace api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "api", Version = "v1" });
             });
+
+            services.AddCors();
+            //services.AddCors(options =>
+            //{
+            //    options.AddPolicy(allowSpecificOrigins, builder =>
+            //     {
+            //         builder.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod();
+ 
+            //     });
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,7 +66,7 @@ namespace api
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(x => x.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod());
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
